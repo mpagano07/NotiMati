f6f12dc4a5279be54958e1c371368b26491cd26a
Matias <paganom@globalhitss.com>
2019-11-14 11:37:39 -0300

    crea db

--- includes/conection.php
+++ includes/conection.php
@@ -3,12 +3,34 @@
 $server   = "localhost";
 $username = "root";
 $password = "Entrar";
-$database = "blog_noticias";
-$db = mysqli_connect($server, $username, $password, $database);
+$database = "blog_noticias2";
+$db = mysqli_connect($server, $username, $password);
 
 mysqli_query($db, "SET NAMES 'uft8'");
+
+if (!$db) {
+    die('Could not connect: ');
+}
+
+// Make my_db the current database
+$db_selected = mysqli_select_db($db, 'blog_noticias2');
+
+if (!$db_selected) {
+  // If we couldn't, then it either doesn't exist, or we can't see it.
+  $sql = 'CREATE DATABASE blog_noticias2';
+
+  if (mysqli_query($db, $sql)) {
+      echo "Database blog_noticias2 created successfully\n";
+  } else {
+      echo 'Error creating database ' . "\n";
+  }
+}
+
+mysqli_close($db);
 
 // inicia la sesion
 if(!isset($_SESSION)){
     session_start();
 }
+
+?>
